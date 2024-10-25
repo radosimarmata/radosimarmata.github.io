7d9bcfeb07ed1a93bc23a613f508a394eb9738a2
radosimarmata27 <simarmatarado@gmail.com>
2024-10-25 11:02:16 +0700

    add typed.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,32 @@
-tailwind.config = {
-  darkMode: 'selector',
-  theme: {
-    extend: {},
-  },
+// tailwind.config = {
+//   darkMode: 'selector',
+//   theme: {
+//     extend: {},
+//   },
+// };
+
+// $('#download-cv').on('click', (e) => {
+//   const link = document.createElement('a');
+//   link.href = './assets/document/CV_RADO_HT_SIMARMATA.pdf';
+//   link.download = 'CV_RADO_HT_SIMARMATA.pdf';
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// });
+
+// document.getElementById("menunav").onclick = function toggleMenu() {
+//   const navToggle = document.getElementsByClassName("toggle");
+//   for (let i = 0; i < navToggle.length; i++) {
+//     navToggle.item(i).classList.toggle("hidden");
+//   }
+// };
+
+
+const options = {
+  strings: ["a Web Developer","an Android Developer","experienced in IoT Development", "a Software Engineer"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
 };
 
-$('#download-cv').on('click', (e) => {
-  const link = document.createElement('a');
-  link.href = './assets/document/CV_RADO_HT_SIMARMATA.pdf';
-  link.download = 'CV_RADO_HT_SIMARMATA.pdf';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-});
-
-document.getElementById("menunav").onclick = function toggleMenu() {
-  const navToggle = document.getElementsByClassName("toggle");
-  for (let i = 0; i < navToggle.length; i++) {
-    navToggle.item(i).classList.toggle("hidden");
-  }
-};
+const typed = new Typed("#typed-output", options);
